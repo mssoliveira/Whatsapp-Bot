2b0d9159d3cfa7738df2aa8a5a90c0fc383c95df
Maurício Souza <mssoliveira1995@gmail.com>
2022-04-26 15:25:43 -0300

    Incluindo novos comandos

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client();
+const client = new Client({
+    authStrategy: new LocalAuth({
+		dataPath: './src/auth/session/'
+	})
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
@@ -11,10 +15,45 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message', message => {
+client.on('message', async (message) => {
 	if(message.body === '!ping') {
-		message.reply('pong');
+		message.reply('🎾 pong');
 	}
+
+	if(message.body === '!me'){
+		message.reply(`Você é ${Math.floor(Math.random() * 101)}% gay.`);
+	}
+
+	if (message.body === '!info') {
+        let info = client.info;
+        client.sendMessage(message.from, `
+            *Connection info*
+            User name: ${info.pushname}
+            My number: ${info.wid.user}
+            Platform: ${info.platform}
+        `);
+    }
+});
+
+client.on('message_create', async (msg) => {
+    // Fired on all message creations, including your own
+    if (msg.fromMe) {        
+		if(msg.body === '!everyone') {
+			const chat = await msg.getChat();
+			
+			let text = "Os Aposentados estão ai: ";
+			let mentions = [];
+
+			for(let participant of chat.participants) {
+				const contact = await client.getContactById(participant.id._serialized);
+				
+				mentions.push(contact);
+				text += `@${participant.id.user} `;
+			}
+
+			await chat.sendMessage(text, { mentions });
+		}
+    }
 });
 
 client.initialize();
