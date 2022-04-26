d869c19e32f11f1f065a242c39dd9613e68e0a14
Maurício Souza <mssoliveira1995@gmail.com>
2022-04-26 11:26:17 -0300

    Initial

---
+++ server.js
@@ -0,0 +1,20 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
+});
+
+client.initialize();
