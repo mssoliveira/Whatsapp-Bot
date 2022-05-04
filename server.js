3ad0ac8ac62e6f5c98d5dbe7ca222d6632d67278
Maurício Souza <mssoliveira1995@gmail.com>
2022-05-04 10:01:45 -0300

    server update

--- server.js
+++ server.js
@@ -2,58 +2,105 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth({
-		dataPath: './src/auth/session/'
-	})
+	authStrategy: new LocalAuth({
+		dataPath: './src/auth/session/',
+	}),
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+client.on('qr', (qr) => {
+	qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', async (message) => {
-	if(message.body === '!ping') {
-		message.reply('🎾 pong');
-	}
-
-	if(message.body === '!me'){
-		message.reply(`Você é ${Math.floor(Math.random() * 101)}% gay.`);
-	}
-
-	if (message.body === '!info') {
-        let info = client.info;
-        client.sendMessage(message.from, `
-            *Connection info*
-            User name: ${info.pushname}
-            My number: ${info.wid.user}
-            Platform: ${info.platform}
-        `);
-    }
-});
-
-client.on('message_create', async (msg) => {
-    // Fired on all message creations, including your own
-    if (msg.fromMe) {        
-		if(msg.body === '!everyone') {
-			const chat = await msg.getChat();
-			
-			let text = "Os Aposentados estão ai: ";
-			let mentions = [];
-
-			for(let participant of chat.participants) {
-				const contact = await client.getContactById(participant.id._serialized);
-				
-				mentions.push(contact);
-				text += `@${participant.id.user} `;
-			}
-
-			await chat.sendMessage(text, { mentions });
-		}
-    }
+	console.log('Client is ready!');
 });
 
 client.initialize();
+
+client.on('message', async (message) => {	
+
+	if (
+		message.body === '#fodasemauricin' ||
+		message.body === '#foda-semauricin'
+	) {
+		message.reply(`Vá tomar no Cu!`);
+	}
+
+	if (message.body === '!me') {
+		message.reply(`Você é ${Math.floor(Math.random() * 101)}% gay.`);
+	}
+
+	if (message.body === 'Boca') {
+		message.reply(`Você é uma Puta`);
+	}	
+});
+
+client.on('message_create', async (msg) => {
+
+	if (msg.body === '!ping') {
+		msg.reply('🎾 pong');
+	}
+
+	if (msg.body === '!todes') {
+		const chat = await msg.getChat();
+
+		let text = '';
+		let mentions = [];
+		
+		for (let participant of chat.participants) {
+			const contact = await client.getContactById(
+				participant.id._serialized
+			);
+
+			mentions.push(contact);
+			text += `@${participant.id.user} `;
+		}
+		await chat.sendMessage(text, { mentions });
+	}
+
+	if (msg.body === '!bomdia') {
+		const chat = await msg.getChat();
+
+		let text = 'Bom Dia a Todos do Grupo!  ';
+		let mentions = [];
+		
+		for (let participant of chat.participants) {
+			const contact = await client.getContactById(
+				participant.id._serialized
+			);
+
+			mentions.push(contact);
+			text += `@${participant.id.user} `;
+		}
+		await chat.sendMessage(text, { mentions });
+	}
+	if (msg.body === '!comedordegalo') {
+		const chat = await msg.getChat();
+		let text = '';
+		let mentions = [];
+		
+		for (let participant of chat.participants) {
+			const contact = await client.getContactById(
+				participant.id._serialized
+			);
+
+			mentions.push(contact);
+			text += `@${participant.id.user} `;
+		}
+		
+		await chat.sendMessage(text, { mentions });
+
+		
+	}
+
+
+	if (msg.fromMe) {
+		if (msg.body === 'Oi') {
+			const contact = await msg.getContact();
+			const chat = await msg.getChat();
+			chat.sendMessage(`Hi @${contact.number}!`, {
+				mentions: [contact],
+			});
+		}
+	}
+});
